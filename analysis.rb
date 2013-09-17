ad811e2acf92def386369f8516c1527f793953d3
Alex Au <alex.au@athenabest.com>
2013-09-17 23:07:53 +0800

    Add tweak data checking

--- analysis.rb
+++ analysis.rb
@@ -7,4 +7,20 @@ imported_results.each do |r|
   results << (2..7).to_a.map{ |x| r[x].to_i }
 end
 
-puts results.detect{ |e| results.count(e) > 1 }
+tweak_results = []
+results.each_with_index do |r, index|
+  puts "Result #{index + 1}: #{r.inspect}"
+  r.each_with_index do |s, i|
+    t = r.clone
+    s += 1
+    s = 1 if s > 49
+    t[i] = s
+    if t.uniq.size == t.size
+      tweak_results << t
+      puts "... Tweak #{i + 1}: #{t.inspect}"
+    end
+  end
+end
+tweak_results << results
+
+puts tweak_results.detect{ |e| tweak_results.count(e) > 1 }.inspect
